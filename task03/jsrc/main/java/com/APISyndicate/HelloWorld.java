32107a5c976859f289e7ade008d73988e82d7524
ab090137 <artem_belmeha@epam.com>
2024-01-23 14:47:48 +0200

    fix task3

--- task03/jsrc/main/java/com/APISyndicate/HelloWorld.java
+++ task03/jsrc/main/java/com/APISyndicate/HelloWorld.java
@@ -9,8 +9,6 @@ import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}"
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
